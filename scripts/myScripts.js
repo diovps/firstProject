27ca0012184ab571700fdfb96cff0173fb465f4f
Nathan Harris <nrharris@unca.edu>
2012-01-31 23:03:58 -0500

    Added contact and About modals with necessary javascript

---
+++ scripts/myScripts.js
@@ -0,0 +1,17 @@
+$(document).ready( function() {
+	$("#contact-btn").click(function() {
+		$("#contact").modal({
+			backdrop: false,
+			keyboard: false,
+			backdrop: false
+		});	
+	});
+
+	$("#about-btn").click(function() {
+		$("#about").modal({
+			backdrop: false,
+			keyboard: false,
+			backdrop: false
+		});
+	});	
+});
